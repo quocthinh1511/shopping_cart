029a8498f8dc2321f9a4d298a3e73fe21da549ac
Quoc Thinh <lquocthinh10a1@gmail.com>
2022-08-31 13:24:18 +0700

    1:23,31/8

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
@@ -8,5 +9,8 @@ Rails.application.routes.draw do
   get 'static_pages/help'
   get 'static_pages/about'
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
