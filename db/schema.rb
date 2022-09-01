c38da5d7fababc71d4920c4e258cc0cbe8ae5146
Quoc Thinh <lquocthinh10a1@gmail.com>
2022-09-01 16:10:19 +0700

    4:10 9/10/2022

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_30_153227) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_31_130052) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -74,6 +74,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_153227) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
