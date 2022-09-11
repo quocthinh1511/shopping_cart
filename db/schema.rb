53c4ed665ca271aaaf3d7f3f70e39109340fdc50
Quoc Thinh <lquocthinh10a1@gmail.com>
2022-09-11 15:37:11 +0700

    11/9/2022, 3:37pm

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_09_071344) do
     t.bigint "order_id"
     t.integer "quantity"
     t.integer "price"
+    t.integer "total"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["order_id"], name: "index_order_items_on_order_id"
@@ -48,9 +49,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_09_071344) do
 
   create_table "orders", force: :cascade do |t|
     t.bigint "user_id"
+    t.bigint "order_status_id"
     t.integer "sum"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["order_status_id"], name: "index_orders_on_order_status_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
